bff1982c17fccc2881a6757ae0e18a045103b081
İlkay Şeki <sekiilkay@gmail.com>
2023-11-03 19:24:31 +0300

    BAYA

---
+++ BAYA.Api/BAYA.Core/Entities/Debris.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAYA.Core.Entities
+{
+    public class Debris : BaseEntity
+    {
+
+
+
+    }
+}
